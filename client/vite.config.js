878c1d6f78690c1de56d5852c87c2f5d3f2654b5
matias indaco <matx.zones@gmail.com>
2026-01-08 20:11:44 -0300

    first commit

---
+++ client/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  //base: "matxcreed.github.io",
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
